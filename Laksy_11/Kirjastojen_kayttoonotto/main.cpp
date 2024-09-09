98ca97170b14d4f804699d1c0a266a246512a39f
Crosyuo <cristian.pukki@gmail.com>
2024-09-09 08:51:45 +0300

    Teht_11

--- Laksy_11/Kirjastojen_kayttoonotto/main.cpp
+++ Laksy_11/Kirjastojen_kayttoonotto/main.cpp
@@ -1,7 +1,90 @@
 #include <iostream>
 #include <SDL.h>
-#include<box2d.h>
+#include <box2d.h>
 
-int main(int argc, char* argv[]) {
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+int main(int argc, char** argv) {
+    SDL_Window* window = nullptr;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL could not be initialized: " << SDL_GetError();
+        return -1;
+    }
+    else {
+        std::cout << "SDL video system is ready to go\n";
+    }
+
+    window = SDL_CreateWindow("Box2D + SDL2 Demo", 500, 200, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Surface* surface = SDL_LoadBMP("./images/rectangle.bmp");
+    SDL_Texture* boxTexture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    b2WorldDef worldDef = b2DefaultWorldDef();
+    worldDef.gravity = { 0.0f, -10.0f };
+    b2WorldId worldId = b2CreateWorld(&worldDef);
+
+    b2BodyDef groundBodyDef = b2DefaultBodyDef();
+    groundBodyDef.position = { 0.0f, -10.0f };
+    b2BodyId groundId = b2CreateBody(worldId, &groundBodyDef);
+    b2Polygon groundBox = b2MakeBox(50.0f, 10.0f);
+    b2ShapeDef groundShapeDef = b2DefaultShapeDef();
+    b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
+
+    b2BodyDef bodyDef = b2DefaultBodyDef();
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position = { 14.0f, 20.0f };
+    b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
+
+    b2Polygon dynamicBox = b2MakeBox(1.0f, 1.0f);
+    b2ShapeDef shapeDef = b2DefaultShapeDef();
+    shapeDef.density = 1.0f;
+    shapeDef.friction = 0.3f;
+    b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    running = false;
+                }
+            }
+        }
+
+        SDL_RenderClear(renderer);
+
+        float timeStep = 1.0f / 60.0f;
+        int subStepCount = 4;   
+        b2World_Step(worldId, timeStep, subStepCount);
+        b2Vec2 position = b2Body_GetPosition(bodyId);
+
+        int x = position.x * 30;
+        int y = SCREEN_HEIGHT - position.y * 30;
+
+        SDL_Rect renderQuad = { x - 50, y - 50, 100, 100 };
+        SDL_RenderCopy(renderer, boxTexture, NULL, &renderQuad);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16);
+    }
+
+    SDL_DestroyTexture(boxTexture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
